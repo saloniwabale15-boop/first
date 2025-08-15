76986f2bda9a40545e392d31107d690ae72b4f34
saloniwabale15-boop <saloniwabale15@gmail.com>
2025-08-15 22:35:38 +0530

    Create sort.cpp

---
+++ sort.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void sort012(vector<int> &arr) {
+    int low = 0, mid = 0, high = arr.size() - 1;
+
+    while (mid <= high) {
+        if (arr[mid] == 0) {
+            swap(arr[low], arr[mid]);
+            low++;
+            mid++;
+        }
+        else if (arr[mid] == 1) {
+            mid++;
+        }
+        else { // arr[mid] == 2
+            swap(arr[mid], arr[high]);
+            high--;
+        }
+    }
+}
+
+int main() {
+    vector<int> arr = {0, 1, 2, 1, 0, 2, 1, 0};
+
+    sort012(arr);
+
+    for (int num : arr) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
