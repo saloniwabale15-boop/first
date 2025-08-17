1dbb4e71f5389345e81d1d541e0ee6261de59a42
saloniwabale15-boop <saloniwabale15@gmail.com>
2025-08-17 17:35:16 +0530

    Update detect.cpp

--- detect.cpp
+++ detect.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 
+int findDuplicate(vector<int>& nums) {
+    // Phase 1: Detect cycle
+    int slow = nums[0];
+    int fast = nums[0];
+
+    do {
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+    } while (slow != fast);
+
+    // Phase 2: Find entrance to the cycle (duplicate number)
+    slow = nums[0];
+    while (slow != fast) {
+        slow = nums[slow];
+        fast = nums[fast];
+    }
+
+    return slow;
+}
+
+int main() {
+    vector<int> arr = {3, 1, 3, 4, 2};
+    cout << "Duplicate number: " << findDuplicate(arr) << endl;
+    return 0;
+}
