94b3a82645914ec6a1f29a28649c846bfc5e091a
saloniwabale15-boop <saloniwabale15@gmail.com>
2025-08-23 12:30:36 +0530

    Create com.cpp

---
+++ com.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+string longestCommonPrefix(vector<string>& strs) {
+    if (strs.empty()) return "";
+
+    string prefix = strs[0]; // take the first word as reference
+
+    for (int i = 1; i < strs.size(); i++) {
+        while (strs[i].find(prefix) != 0) { 
+            // shrink prefix until it matches the start of strs[i]
+            prefix = prefix.substr(0, prefix.size() - 1);
+            if (prefix.empty()) return "";
+        }
+    }
+    return prefix;
+}
+
+int main() {
+    vector<string> strs = {"flower", "flow", "flight"};
+    cout << "Longest Common Prefix: " << longestCommonPrefix(strs) << endl;
+    return 0;
+}
